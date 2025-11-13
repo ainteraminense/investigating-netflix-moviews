e145d73433752a6b10c5d6d5cb20bfd2ea50c9bb
ainteraminense <99498988+ainteraminense@users.noreply.github.com>
2025-11-13 16:24:45 -0500

    Count the number of short action movies from the 1990s

--- netflix_movies.py
+++ netflix_movies.py
@@ -8,4 +8,15 @@ movies = netflix_df[netflix_df["type"] == "Movie"]
 movies_1990s = movies[np.logical_and(movies["release_year"] >= 1990, movies["release_year"] <= 1999)]
 # Vizualize distribution
 plt.hist(movies_1990s["duration"])
-plt.show() # 120 min most frequent 47 times
+plt.title('Distribution of Movie Durations in the 1990s')
+plt.xlabel('Duration (min)')
+plt.ylabel("Number of Movies")
+plt.show() 
+duration = 100 # 100-120 min most frequent 47 times
+# Count the number of short action movies from the 1990s
+action_movies_1990s = movies_1990s[movies["genre"] == "Action"]
+short_movie_count = 0
+for movie_duration in action_movies_1990s["duration"]:
+    if movie_duration < 90:
+        short_movie_count += 1
+print(short_movie_count)
